d5f4f12bf9c68c97d90c983cba852c3e104d050d
kaixuan <kaixuan@acepointer.sg>
2022-06-17 00:49:29 +0800

    vector icons package install

--- app/screens/ContactsScreen.js
+++ app/screens/ContactsScreen.js
@@ -1,10 +1,17 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React, {useContext} from 'react';
+import React, {useContext, useLayoutEffect} from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import ContactsList from '../components/ContactsList';
 import PersonContext from '../contexts/PersonContext';
 
-const ContactsScreen = () => {
+const ContactsScreen = ({navigation}) => {
   const {persons} = useContext(PersonContext);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => <Icon name="add" />,
+    });
+  }, [navigation]);
 
   return (
     <View style={styles.container}>
